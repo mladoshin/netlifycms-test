3f408936a556d6d2ea3b45bd77d59f37e90fe1ee
Maxim Ladoshin <mladoshin@mail.ru>
2023-01-19 09:19:18 +0300

    bugfix

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,6 @@ module.exports = {
     },
   },
   plugins: [
-    `gatsby-plugin-netlify-cms`
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -122,5 +121,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-netlify-cms`
   ],
 }
